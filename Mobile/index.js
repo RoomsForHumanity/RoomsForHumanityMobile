9fb7b4208346d1c9627c4b91a5bbd0bd4e947ec3
Jocelyn Ragukonis <jnragu@gmail.com>
2018-06-26 13:05:54 -0400

    Moved node to mobile folder

--- Mobile/index.js
+++ Mobile/index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View, Text } from 'react-native';
+
+const App = () => (
+    <View>
+      <Text> Hello </Text>
+    </View>
+);
 
 AppRegistry.registerComponent('Mobile', () => App);
